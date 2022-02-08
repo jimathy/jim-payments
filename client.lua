be931570bb57b662322b5c449f2c378073191b63
jimathy <gothicjim@gmail.com>
2022-02-08 19:19:05 +0000

    Re-write
    Had to rewrite the code for grabbing the player list
    
    All on client side now and no callbacks

--- client.lua
+++ client.lua
@@ -28,7 +28,7 @@ if GetCurrentResourceName() == resource then QBCore.Functions.GetPlayerData(func
 Citizen.CreateThread(function()
 	local jobroles = {}
 	for k, v in pairs(Config.Jobs) do jobroles[tostring(k)] = 0 end
-	exports['qb-target']:AddBoxZone("JimBank", vector3(251.75, 222.17, 106.2), 0.6, 2.0, { name="JimBank", heading = 340.0, debugPoly=false, minZ = 105.75, maxZ = 107.29, }, 
+	exports['qb-target']:AddBoxZone("JimBank", vector3(251.75, 222.17, 106.2), 0.6, 2.0, { name="JimBank", heading = 340.0, debugPoly=true, minZ = 105.75, maxZ = 107.29, }, 
 		{ options = { { event = "jim-payments:Tickets:Menu", icon = "fas fa-receipt", label = "Cash in Receipts", job = jobroles } },
 		  distance = 2.0
 	})
@@ -36,27 +36,32 @@ end)
 
 RegisterNetEvent('jim-payments:client:Charge', function()
 	if not onDuty then TriggerEvent("QBCore:Notify", "Not Clocked in!", "error") return end  -- Require to be on duty when making a payment
-    QBCore.Functions.TriggerCallback('jim-payments:server:GetCurrentPlayers', function(playerList)
-	if playerList[1] == nil then TriggerEvent("QBCore:Notify", "No one near by to charge", "error") return end
-		local dialog = exports['qb-input']:ShowInput({ header = "Charge Customer", submitText = "Send",
-		inputs = {
-				{ text = "Citizen",	name = "citizen", type = "select", options = playerList	},				
-				{ type = 'number', isRequired = true, name = 'price', text = 'Amount to Charge' },
-				{ type = 'radio', name = 'billtype', text = 'Payment Type', options = {	{ value = "cash", text = "Cash" }, { value = "card", text = "Card" } } }, }
-		})
-		if dialog then
-			if not dialog.citizen or not dialog.price then return end
-			TriggerServerEvent('jim-payments:server:Charge', dialog.citizen, dialog.price, dialog.billtype)
+	local playerList = {}
+	for k,v in pairs(QBCore.Functions.GetPlayersFromCoords(GetEntityCoords(PlayerPedId()), Config.PaymentRadius)) do
+		dist = #(GetEntityCoords(GetPlayerPed(v)) - GetEntityCoords(PlayerPedId()))
+		TriggerEvent("QBCore:Notify", "["..GetPlayerServerId(v).."] - "..GetPlayerName(v).." ("..math.floor(dist+0.5).."m)", "error")
+		if v ~= PlayerId() then
+			playerList[#playerList+1] = { value = GetPlayerServerId(v), text = "["..GetPlayerServerId(v).."] - "..GetPlayerName(v).." ("..math.floor(dist+0.5).."m)" }
 		end
-	end)
+	end    
+	if playerList[1] == nil then TriggerEvent("QBCore:Notify", "No one near by to charge", "error") return end
+	local dialog = exports['qb-input']:ShowInput({ header = "Charge Customer", submitText = "Send",
+	inputs = {
+			{ text = "Citizen", name = "citizen", type = "select", options = playerList    },                
+			{ type = 'number', isRequired = true, name = 'price', text = 'Amount to Charge' },
+			{ type = 'radio', name = 'billtype', text = 'Payment Type', options = { { value = "cash", text = "Cash" }, { value = "card", text = "Card" } } }, }
+	})
+	if dialog then
+		if not dialog.citizen or not dialog.price then return end
+		TriggerServerEvent('jim-payments:server:Charge', dialog.citizen, dialog.price, dialog.billtype)
+	end
 end)
-
 RegisterNetEvent('jim-payments:Tickets:Menu', function()
 	for k, v in pairs(Config.Jobs) do if k ~= PlayerJob.name then 
 		else exports['qb-menu']:openMenu({
-				{ isMenuHeader = true, header = PlayerJob.label.." Ticket Payment", txt = "Do you want trade your tickets for payment?" },
-				{ header = "Yes", txt = "", params = { event = "jim-payments:Tickets:Sell:yes" } },
-				{ header = "No", txt = "", params = { event = "jim-payments:Tickets:Sell:no" } }, })
+			{ isMenuHeader = true, header = PlayerJob.label.." Ticket Payment", txt = "Do you want trade your tickets for payment?" },
+			{ header = "Yes", txt = "", params = { event = "jim-payments:Tickets:Sell:yes" } },
+			{ header = "No", txt = "", params = { event = "jim-payments:Tickets:Sell:no" } }, })
 		end
 	end
 end)

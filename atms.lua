f16a0b0d1f45a502bcdda77e88e2ea8316dd006e
jimathy <gothicjim@gmail.com>
2022-03-17 10:57:14 +0000

    Removed loops, it was being too strict

--- atms.lua
+++ atms.lua
@@ -140,33 +140,27 @@ local bossroles = {}
 local gangroles = {}
 Citizen.CreateThread(function()
 	if Config.useATM or Config.useBanks then
-		while bossroles == nil do
-			for k, v in pairs(QBCore.Shared.Jobs) do ---Grabs the list of jobs
-				for l, b in pairs(QBCore.Shared.Jobs[tostring(k)].grades) do -- Grabs the list of grades
-					if QBCore.Shared.Jobs[tostring(k)].grades[l].isboss == true then -- Checks the grade if is boss
-						if bossroles[tostring(k)] ~= nil then -- checks if the line exists
-							if bossroles[tostring(k)] > tonumber(l) then bossroles[tostring(k)] = tonumber(l) end -- the 
-						else bossroles[tostring(k)] = tonumber(l)
-						end
+		for k, v in pairs(QBCore.Shared.Jobs) do ---Grabs the list of jobs
+			for l, b in pairs(QBCore.Shared.Jobs[tostring(k)].grades) do -- Grabs the list of grades
+				if QBCore.Shared.Jobs[tostring(k)].grades[l].isboss == true then -- Checks the grade if is boss
+					if bossroles[tostring(k)] ~= nil then -- checks if the line exists
+						if bossroles[tostring(k)] > tonumber(l) then bossroles[tostring(k)] = tonumber(l) end -- the 
+					else bossroles[tostring(k)] = tonumber(l)
 					end
 				end
 			end
-			Wait(100)
 		end
-		while gangroles == nil do
-			for k, v in pairs(QBCore.Shared.Gangs) do 
-				for l, b in pairs(QBCore.Shared.Gangs[tostring(k)].grades) do
-					if tostring(k) ~= "none" then
-						if QBCore.Shared.Gangs[tostring(k)].grades[l].isboss == true then
-							if gangroles[tostring(k)] ~= nil then
-								if gangroles[tostring(k)] > tonumber(l) then gangroles[tostring(k)] = tonumber(l) end
-							else gangroles[tostring(k)] = tonumber(l)
-							end
+		for k, v in pairs(QBCore.Shared.Gangs) do 
+			for l, b in pairs(QBCore.Shared.Gangs[tostring(k)].grades) do
+				if tostring(k) ~= "none" then
+					if QBCore.Shared.Gangs[tostring(k)].grades[l].isboss == true then
+						if gangroles[tostring(k)] ~= nil then
+							if gangroles[tostring(k)] > tonumber(l) then gangroles[tostring(k)] = tonumber(l) end
+						else gangroles[tostring(k)] = tonumber(l)
 						end
-					end	
-				end
+					end
+				end	
 			end
-			Wait(100)
 		end
 	end
 	if Config.useATM then

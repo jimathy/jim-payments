7a24a8836d911780256f4507ba513cb4edaf9089
jimathy <gothicjim@gmail.com>
2022-04-24 11:08:22 +0100

    Debug default

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ print("Jim-Payments v2.4 - Payments Script by Jimathy")
 
 Config = {}
 
-Config.Debug = true
+Config.Debug = false
 
 Config.Manage = true -- "true" if using qb-management
 					 -- "false" if using qb-bossmenu

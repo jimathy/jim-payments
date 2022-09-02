97b54cd4823e02304a7d505edfa74f3c44c4ebf5
Vosscat <63515989+vosscat@users.noreply.github.com>
2022-09-02 21:03:02 +0800

    Locale Support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,6 @@ fx_version "cerulean"
 game "gta5"
 
 dependencies { 'qb-input', 'qb-target', }
-client_scripts { 'client/*.lua' }
+client_scripts { 'client/*.lua', 'locales/*.lua' }
 server_scripts { '@oxmysql/lib/MySQL.lua', 'server/*.lua' }
-shared_scripts { 'config.lua', 'shared/*.lua' }
+shared_scripts { 'config.lua', 'shared/*.lua', 'locales/*.lua' }

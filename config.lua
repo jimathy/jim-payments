4a28bea8109509806420a8c34db99caee65ba465
taylorrenee1 <120302939+taylorrenee1@users.noreply.github.com>
2024-01-06 21:36:23 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,6 +8,7 @@ Config = {
 	Lan = "en",
 	Debug = false,
 	Notify = "qb",		-- "qb" | "okok" | "t" | "infinity" | "rr"
+	Menu = "ox"
 
 	---------------------------------
 	-- Default Job Payment Systems --

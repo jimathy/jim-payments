e1870b92b9a81e51edb3a4bbfa47a534f1b029e1
taylorrenee1 <120302939+taylorrenee1@users.noreply.github.com>
2024-01-06 21:36:38 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,3 +9,4 @@ lua54 'yes'
 shared_scripts { 'config.lua', 'shared/*.lua', 'locales/*.lua' }
 client_scripts { 'client/*.lua', }
 server_scripts { '@oxmysql/lib/MySQL.lua', 'server/*.lua' }
+shared_scripts { '@ox_lib/init.lua','config.lua', 'shared/*.lua', 'locales/*.lua' }
